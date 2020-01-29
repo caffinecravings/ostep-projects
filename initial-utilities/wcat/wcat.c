8ed4e1e3b70a90ff92d9249010bb66f200599225
caffine <34240605+caffinecravings@users.noreply.github.com>
2020-01-29 16:06:20 -0500

    add wcat.c

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,5 +1,6 @@
 /*	wcat.c
-	a command line program, in the directory wcat use to read files
+	a command line program, in the directory wcat use to read files line
+	by line
         ex -    to run wcat on test.txt
                 ./wcat test.txt
 
@@ -12,22 +13,22 @@
 
 int main(int agrc, char *argv[]){
 	int a = agrc;
-	if(1<a){
-		int i = 0, size = 10000;
+	if(1<a){ // argv[0] is system call wcat
+		int i = 1, size = 1000;
 		char line[size];
-		while(i<a){
-			FILE *f = fopen(argv[i], "r");
-			if(f == NULL) printf("wcat cannot open file\n");
-			else{
+		while(i<a){ // while only 2 or more args, no loop if only wcat given
+                        FILE *f = fopen(argv[i], "r");
+                        if(f == NULL) printf("wcat cannot open %s\n", argv[i]);
+                        else{
 				while( fgets(line, size, f) ){
 					printf("%s\n", line);
 				}
 			}
 			i++;
-			fclose(f);
+			if( f != NULL ) fclose(f); // throws err when argv is not a file
 		}
 	}
-	else{
+	else{ // technically false, no args besides wcat
 		printf("No arguments given\n");
 	}
 	return 0;
